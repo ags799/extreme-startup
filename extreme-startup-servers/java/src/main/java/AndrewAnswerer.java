7c8866f06d42784cfd0e427c1c7f174eaea684ae
Andrew Sharp <andrew.sharp57@gmail.com>
2017-02-23 16:40:02 -0500

    maximum

--- extreme-startup-servers/java/src/main/java/AndrewAnswerer.java
+++ extreme-startup-servers/java/src/main/java/AndrewAnswerer.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+
 public class AndrewAnswerer {
     public static String getName() {
         return "merlions";
     }
 
     public static boolean questionIsLargest(String realQuestion) {
+        return realQuestion.contains("largest");
+    }
+
+    public static String getQuestionIsLargest(String realQuestion) {
+        return "" + Arrays.asList(Arrays.asList(realQuestion.split(": ")).get(1).split(", ")).stream().mapToInt(Integer::parseInt).max().getAsInt();
     }
 }
